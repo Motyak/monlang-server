497650454bf8fa36f76c6406b268ffe1b3c2917f
Motyak <m0tyak_@live.fr>
2025-06-25 12:52:50 +0200

    fix bugs

--- eval.sh
+++ eval.sh
@@ -15,9 +15,9 @@ mkdir -p home/{out,"$srcdir"}
 touch home/out/console.txt
 cp "$tmppath" "home/$srcpath"
 cd home
->/dev/null 2>&1 ../monlang-parser/bin/main.elf "$srcpath" & parser_pid=$!
->out/console.txt 2>&1 ../monlang-interpreter/bin/main.elf "$srcpath" & interpreter_pid=$!
-
+&>/dev/null ../monlang-parser/bin/main.elf "$srcpath" & parser_pid=$!
+# we need to use stdbuf otherwise we won't get output in case the program segfaults, etc..
+&>out/console.txt stdbuf -oL ../monlang-interpreter/bin/main.elf "$srcpath" & interpreter_pid=$!
 set +o errexit; trap - ERR
 wait -n $parser_pid; parser_exit_code=$?
 wait -n $interpreter_pid; interpreter_exit_code=$?
